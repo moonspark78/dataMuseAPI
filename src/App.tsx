1a0b3a3dfb8825460c66cc77a2fe30fa9be4c712
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-27 00:16:05 +0200

    :safety_vest: :safety_vest: :safety_vest:  :art:

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+type Synonym = {
+  word: string;
+  score: number;
+}
 
 function App() {
   const [word, setWord] = useState("")
+  const [synonyms, setSynonyms] = useState<Synonym[]>([])
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
     fetch(`https://api.datamuse.com/words?rel_syn=fast`)
+      .then((res) => res.json())
+      .then(setSynonyms)
   };
   return (
     <div className="App">
       
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='word-input'>Your Word</label>
         <input 
-          onSubmit={handleSubmit}
           value={word}
           id='word-input'
           onChange={(e) => setWord(e.target.value)}
         ></input>
         <button>Submit</button>
       </form>
+      {
+        synonyms.map((synonym) => <li>
+        {synonym.word}
+        </li>)
+      }
     </div>
   );
 }
